038557fc8743a95a65b17245c0a331e6db3f33b7
SEONGJOON KIM <jobcho6320@naver.com>
2022-10-17 00:35:03 +0900

    Add: 랜덤 숫자 섞기

--- src/MyClass.java
+++ src/MyClass.java
@@ -1,5 +1,10 @@
 public class MyClass {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        int[] score = new int[6];
+        for (int i = 0; i < score.length; i++) {
+            score[i] = i;
+            System.out.println(score[i]);
+        }
+
     }
 }
